5609ed7b152d0789ef9b149576e5c8e631b50b48
NickMavrias <79319666+NickMavrias@users.noreply.github.com>
2021-05-22 13:31:19 +0300

    Update RequestDonationList.java

--- RequestDonationList.java
+++ RequestDonationList.java
@@ -5,41 +5,41 @@ public class RequestDonationList {
     ArrayList<RequestDonation> rdEntities = new ArrayList<>();
 
     //methods
-     public RequestDonation get(Entity id){
-          return rdEntities.get(id.getId());
-     }
+    public RequestDonation get(Entity id){
+        return rdEntities.get(id.getId());
+    }
 
-     public void add(RequestDonation RD){
-         rdEntities.add(RD);
-         if(rdEntities.contains(RD)){
-             RequestDonation RD1 = new RequestDonation();
-             rdEntities.add(RD1);
-         }
-         else try{
-         }
-         catch(Exception e){
-             System.out.println("Ekanes malakia");
-         }
-     }
+    public void add(RequestDonation RD){
+        rdEntities.add(RD);
+        if(rdEntities.contains(RD)){
+            RequestDonation RD1 = new RequestDonation();
+            rdEntities.add(RD1);
+        }
+        else try{
+        }
+        catch(Exception e){
+            System.out.println("Ekanes malakia");
+        }
+    }
 
-     public void remove(RequestDonation RD){
-         rdEntities.remove(RD);
-     }
+    public void remove(RequestDonation RD){
+        rdEntities.remove(RD);
+    }
 
-     public void modify(RequestDonation RD){
-         RD.setQuantity(RD.getQuantity());
-     }
+    public void modify(RequestDonation RD){
+        RD.setQuantity(RD.getQuantity());
+    }
 
-     public void monitor(){
-         for(RequestDonation i: rdEntities){
-             System.out.print(i);
-         }
-         System.out.println("");
-     }
+    public void monitor(){
+        for(RequestDonation i: rdEntities){
+            System.out.print(i);
+        }
+        System.out.println("");
+    }
 
-     public void reset(){
-         rdEntities.clear();
-     }
+    public void reset(){
+        rdEntities.clear();
+    }
 
 
 }
